a6794652404d0732063dcd2d6b96c78d26afc682
lucascody <lucassantiagocody@gmail.com>
2024-07-18 23:10:16 +0100

    Create User model class

--- user/models.py
+++ user/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import BaseUserManager
-
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
 
 class UserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
@@ -20,3 +20,18 @@ class UserManager(BaseUserManager):
         if not extra_fields.get('is_superuser'):
             raise ValueError('is_superuser field must be set')
         self.create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=False)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = ('email')
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
