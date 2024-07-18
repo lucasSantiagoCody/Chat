7694f3be084d4910fb8b95628ac5f292d9d104a9
lucascody <lucassantiagocody@gmail.com>
2024-07-18 23:09:38 +0100

    Create UserManager class

--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email field must be set')
+
+        normalized_email = self.normalize_email(email)
+        user = self.model(username=username, email=normalized_email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if not extra_fields.get('is_staff'):
+            raise ValueError('is_staff field must be set')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('is_superuser field must be set')
+        self.create_user(username, email, password, **extra_fields)
