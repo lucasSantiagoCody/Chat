de8177f5cdbbb263f9a29d440fdb71bc4e724ea8
lucascody <lucassantiagocody@gmail.com>
2024-07-18 23:13:53 +0100

    Create UserAdmin model admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(ModelAdmin):
+    model = User
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ['username', 'is_staff', 'is_superuser']
+    list_filter = ['email', 'is_superuser']
+
+admin.site.register(User, UserAdmin)
