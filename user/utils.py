fd1d9c638475f8e33d0b6f81af85e919cbaf7a9f
lucascody <lucassantiagocody@gmail.com>
2024-07-19 11:32:27 +0100

    Create email_field_validator function validator

--- user/utils.py
+++ user/utils.py
@@ -1,4 +1,16 @@
+from .models import User
+import re
+
 def username_field_validator(username):
     if username and len(username) >= 3:
         return True
+    return False
+
+def email_field_validator(email):
+    email_regex_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+    if re.fullmatch(email_regex_pattern, email):
+        exists_email = User.objects.filter(email=email)
+        if not exists_email:
+            return True
     return False
