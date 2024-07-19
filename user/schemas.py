701042c9cbea1453844f7ec4b988692901d47c64
lucascody <lucassantiagocody@gmail.com>
2024-07-19 21:35:30 +0100

    Create LoginSchema

--- user/schemas.py
+++ user/schemas.py
@@ -1,3 +1,4 @@
+from ninja.schema import Schema
 from ninja import ModelSchema
 from .models import User
 
@@ -5,3 +6,7 @@ class UserSchema(ModelSchema):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+class LoginSchema(Schema):
+    email: str
+    password: str
