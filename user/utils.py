b0a5b0c359f83fd655cf54bdf431824ccba3a7d4
lucascody <lucassantiagocody@gmail.com>
2024-07-19 13:32:34 +0100

    Create register_request_validator request function validator

--- user/utils.py
+++ user/utils.py
@@ -1,6 +1,30 @@
+from core.utils import error_message_handler
+from ninja.errors import HttpError
 from .models import User
 import re
 
+
+def register_request_validator(request_data):
+    check_username = username_field_validator(request_data.username)
+    check_email = email_field_validator(request_data.email)
+    check_password = password_field_validator(request_data.password)
+
+    messages_error = []
+
+    if not check_username:
+        messages_error.append('Invalid username field')
+
+    if not check_email:
+        messages_error.append('Invalid email field')
+
+    if not check_password:
+        messages_error.append('Invalid password field min 6 characters')
+
+    single_msg = error_message_handler(messages_error)
+    if single_msg:
+        raise HttpError(status_code=400, message=single_msg)
+    
+    return True
 
 def username_field_validator(username):
     if len(username.strip()) >= 3:
