ce89e545832e92db88ca07cc6fa6ec55238b56a8
lucascody <lucassantiagocody@gmail.com>
2024-07-19 21:36:51 +0100

    Create login api view

--- user/api.py
+++ user/api.py
@@ -1,13 +1,16 @@
+from django.contrib.auth import authenticate, login as auth_login
 from .utils import register_request_validator
 from ninja.errors import HttpError
+from .schemas import LoginSchema
 from ninja.router import Router
 from .schemas import UserSchema
 from .models import User
 
 router = Router()
 
+_LOGIN_BACKEND = 'django.contrib.auth.backends.ModelBackend'
 
-@router.post("register/", response={201: dict})
+@router.post('register/', response={201: dict})
 def register(request, data:UserSchema):
     register_request_validator(data)
     try:
@@ -20,3 +23,13 @@ def register(request, data:UserSchema):
         return {'message': 'registered user successfully'}
     except:
         raise HttpError(status_code=500, message='Internal server error')
+    
+
+@router.post('login/', response={200: dict, 401: str}, auth=None)
+def login(request, data:LoginSchema):
+    user = authenticate(backend=_LOGIN_BACKEND, **data.dict())
+    if user and user.is_active:
+        auth_login(request, user, backend=_LOGIN_BACKEND)
+        return 200, {'message': 'Login successfully'}
+    else:
+        return 401, "Access denied"
