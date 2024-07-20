906b2a919e414ddfe3bc6ef76c80ed1bf519a0ad
lucascody <lucassantiagocody@gmail.com>
2024-07-20 20:52:33 +0100

    Add CHANNEL_LAYERS setting in core's settings

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CHANNEL_LAYERS = {
+    "defaulf": {
+        'BACKEND':'channels_redis.core.RedisChannelLayer',
+        'CONFIG':{
+            'host': config('REDIS_HOST'),
+            'port': config('REDIS_PORT')
+        }
+    }
+}
