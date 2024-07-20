c79d2661fbf239e27b5e5f650dc66a95001c958c
lucascody <lucassantiagocody@gmail.com>
2024-07-20 23:47:36 +0100

    Add async connect function in ChatConsumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,4 +3,11 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    ...
+
+    async def connect(self):
+        self.room_group_name = 'group_chat'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
