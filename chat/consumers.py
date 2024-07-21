948f2af21de4f2b2f793591a70027573759089a2
lucascody <lucassantiagocody@gmail.com>
2024-07-22 00:43:49 +0100

    Update async connect ChatConsumer function

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,8 +5,7 @@ import json
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        self.room_group_name = 'chat'
 
         await self.channel_layer.group_add(
             self.room_group_name,
