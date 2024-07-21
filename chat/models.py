952769e5d16e50ecc804de9d3ced9b1e2fd887b2
lucascody <lucassantiagocody@gmail.com>
2024-07-21 16:51:05 +0100

    Update Room model save method

--- chat/models.py
+++ chat/models.py
@@ -1,11 +1,22 @@
+from .utils import generate_random_string
 from django.db import models
 
 
+
 class Room(models.Model):
-    token = models.CharField(max_length=255, unique=True)
+    token = models.CharField(max_length=255, unique=True, blank=True)
     room = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = generate_random_string(30)
+
+        return super(Room, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.room
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
