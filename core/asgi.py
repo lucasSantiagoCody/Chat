685c92b9d1ad45663f58a45a00dd6e0a61f13d33
lucascody <lucassantiagocody@gmail.com>
2024-07-22 00:48:35 +0100

    Update asgi.py file to accept websocket requests

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,19 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
+from channels.routing import ProtocolTypeRouter , URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from chat import routing
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http" : get_asgi_application() , 
+        "websocket" : AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )    
+        )
+    }
+)
