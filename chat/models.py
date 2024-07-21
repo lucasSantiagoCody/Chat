dd9f576daf52096d4502447d3d477b0e9d91dd97
lucascody <lucassantiagocody@gmail.com>
2024-07-21 15:40:35 +0100

    Add more fields in Room model

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Room(models.Model):
+    token = models.CharField(max_length=255, unique=True)
     room = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
