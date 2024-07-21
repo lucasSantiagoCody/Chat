466183ff771edc932fc7556ff2e40ba3fcf2b8b8
lucascody <lucassantiagocody@gmail.com>
2024-07-21 01:10:19 +0100

    Create Message model

--- chat/models.py
+++ chat/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class Room(models.Model):
     room = models.CharField(max_length=50)
-    
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
