49fb83d5933a2417433cb7243207d68c9c46e481
lucascody <lucassantiagocody@gmail.com>
2024-07-21 01:03:13 +0100

    Create chat's api endpoint

--- core/api.py
+++ core/api.py
@@ -1,7 +1,9 @@
 from ninja import NinjaAPI
 from user.api import router as users_router
+from chat.api import router as chat_routers
+
 
 api = NinjaAPI()
 
 api.add_router('user/', users_router)
-
+api.add_router('chat/', chat_routers)
