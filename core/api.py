fde89b81f5377867ae44ed580a332b1ab78cddd6
lucascody <lucassantiagocody@gmail.com>
2024-07-21 01:05:32 +0100

    Rename Router instances users_router and chat_routers to user_router and chat_router

--- core/api.py
+++ core/api.py
@@ -1,9 +1,9 @@
 from ninja import NinjaAPI
-from user.api import router as users_router
-from chat.api import router as chat_routers
+from user.api import router as user_router
+from chat.api import router as chat_router
 
 
 api = NinjaAPI()
 
-api.add_router('user/', users_router)
-api.add_router('chat/', chat_routers)
+api.add_router('user/', user_router)
+api.add_router('chat/', chat_router)
